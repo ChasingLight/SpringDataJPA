1b618e91b28ad2a5a63e9931f50112fc6e78e5f3
jaden <657091714@qq.com>
2018-12-19 10:02:56 +0800

    1.上传  2018-12-19 jinhd

---
+++ src/main/java/AQS/exlusiveMode/MutexTest.java
@@ -0,0 +1,24 @@
+package AQS.exlusiveMode;
+
+
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class MutexTest {
+
+    public static void main(String[] args) {
+
+        Sale sale = new Sale();
+
+        Thread t1=new Thread(sale);
+        Thread t2=new Thread(sale);
+        Thread t3=new Thread(sale);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+
+    } //end method
+}
---
+++ src/main/java/AQS/exlusiveMode/Sale.java
@@ -0,0 +1,34 @@
+package AQS.exlusiveMode;
+
+class Sale  implements Runnable {
+
+    private final Mutex mutexLock = new Mutex();
+
+    private int count = 200; //车票总数
+
+    @Override
+    public void run() {
+        while(true){
+            //mutexLock.lock();
+
+            try{
+                if (count > 0){
+
+                    Thread.sleep(100);  //模拟购票等待时间
+
+                    System.out.println(Thread.currentThread().getName() + "当前售卖的票的编号：" + count);
+                    count--;
+                }else{
+                    System.out.println("车票已经售卖完毕");
+                    return;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            finally{
+                //mutexLock.unlock();
+            }
+        }
+    }
+
+}//end class
